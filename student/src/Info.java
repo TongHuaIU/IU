e3a0eff17efeb67f0ad2349a0f037317e049ebc7
TongHuaIU <1289622118@qq.com>
2022-07-27 09:47:31 +0800

    123456

---
+++ student/src/Info.java
@@ -0,0 +1,30 @@
+public class Info {
+    public String goToNightClasses,goToLibrary,goToClasses,goRadioGymnastics,doMath;
+
+
+    /**
+     *行为
+     *上课
+    *上晚自习
+    *泡图书馆
+    *做广播体操
+    *f(x) = ax+b
+    **/
+
+
+    public void goToNightClasses() {
+        System.out.println("我们在认真的上晚自习");
+    }
+    public void goToClasses(){
+        System.out.println("我们在上课");
+    }
+    public void goToLibrary(){
+        System.out.println("我们在泡图书馆");
+    }
+    public void goRadioGymnastics(){
+        System.out.println("我们做广播体操");
+    }
+    public void doMath(){
+        System.out.println("做数学题");
+    }
+}
